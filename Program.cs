8c813d69e399ec6f21e7ddd4ea55aa6be8e58ec2
jcallies3 <jcallies3@my.wctc.edu>
2025-11-11 14:15:36 -0600

    [1.16] Display categories and related products

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ do
   Console.WriteLine("1) Display categories");
   Console.WriteLine("2) Add category");
   Console.WriteLine("3) Display Category and related products");
+  Console.WriteLine("4) Display all Categories and their related products");
   Console.WriteLine("Enter to quit");
   string? choice = Console.ReadLine();
   
@@ -100,9 +101,23 @@ do
         logger.Info($"CategoryId {id} selected");
         Category category = db.Categories.Include("Products").FirstOrDefault(c => c.CategoryId == id)!;
         Console.WriteLine($"{category.CategoryName} - {category.Description}");
-         foreach (Product p in category.Products)
+        foreach (Product p in category.Products)
         {
-        Console.WriteLine($"\t{p.ProductName}");
+            Console.WriteLine($"\t{p.ProductName}");
+        }
+    }
+
+    else if (choice == "4")
+    {
+        var db = new DataContext();
+        var query = db.Categories.Include("Products").OrderBy(p => p.CategoryId);
+        foreach (var item in query)
+        {
+            Console.WriteLine($"{item.CategoryName}");
+            foreach (Product p in item.Products)
+            {
+                Console.WriteLine($"\t{p.ProductName}");
+            }
         }
     }
 
